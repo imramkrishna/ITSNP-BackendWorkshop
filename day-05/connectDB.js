16011ad94218705056c10143d1a895d8b9f17ac7
Ram Krishna Yadav <itsramky234@gmail.com>
2025-07-30 22:33:34 +0545

    completed day-05

--- day-05/connectDB.js
+++ day-05/connectDB.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 async function connectDB() {
-    const connectionString = "mongodb+srv://itsramky234:krishna@cluster0.xpocy3x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    mongoose.connect(connectionString)
-        .then(() => console.log("Connected to Database"))
+    const connectionString = "your_mongoDB_string"
         .catch(() => {
             console.log("error while connecting to databse.")
         })
